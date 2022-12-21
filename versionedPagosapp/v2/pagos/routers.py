87877041fee1a908873917539feb252e33b17315
WilliamMiguel <113279478+WilliamMiguel@users.noreply.github.com>
2022-12-20 22:45:43 -0500

    Antes de version 2

--- versionedPagosapp/v2/pagos/routers.py
+++
@@ -1,9 +0,0 @@
-from pagos.api import ServicesViewSet, ExpiredPaymentsViewSet, PaymentUserViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'payment', PaymentUserViewSet, basename="payment")
-router.register(r'services', ServicesViewSet, basename="services")
-router.register(r'expired-payments', ExpiredPaymentsViewSet, basename="expired-payments")
-
-api_urlpatterns = router.urls
